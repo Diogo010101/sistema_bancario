188aee1dad330dd288f9f6c7662e045f24d676a7
Diogo010101 <diogospk@hotmail.com>
2024-10-16 01:10:49 -0300

    Adicionados alguns novos textos e import do sleep para deixar o funcionamento do programa mais parecido com uma transação real

--- taa.py
+++ taa.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 menu = f'''\n{'-' *5}BANCO SAQUE MIL{'-' *5}
 
 [D] Depósito
@@ -62,4 +63,84 @@ while True:
         break
 
     else:
+=======
+from time import sleep
+
+menu = f'''\n{'-' *5}BANCO SAQUE MIL{'-' *5}
+
+[D] Depósito
+[S] Sacar
+[E] Extrato
+[Q] Sair
+
+=>'''
+saldo = 0
+limite = 500
+extrato = {}
+numero_saque = 0
+limite_saque = 3
+numero_deposito = 0
+
+
+while True:
+
+    opcao = str(input(menu))
+
+    if opcao.upper() == 'D':
+        deposito= int(input('Digite o valor que deseja depositar: '))
+        if deposito > 0:
+            print(f'Realizando deposito de {deposito:.2f}')
+            sleep(3)
+            numero_deposito += 1
+            saldo += deposito
+            chave = f'Deposito: {numero_deposito}'
+            valor = f"R$ {deposito:.2f}"
+            extrato.update({chave:valor})
+            print(f'Depósito de R${deposito:.2f} realizado com sucesso!\nSaldo atualizado: R${saldo:.2f}')
+            sleep(1)
+
+
+        else:
+            print('O valor deve sar maior do que "0"')
+
+
+    elif opcao.upper() == 'S':
+        saque = int(input('Qual valor deseja sacar? [Limite de 500 por saque]>'))
+        if numero_saque >= 3:
+            print('Você atingiu o limite de saques por dia')
+        elif saque > 500:
+            print('O valor máximo de saque por transação é do R$500')
+        elif saque <= 0:
+            print('Não é possível sacar um valor menor ou igual a 0')
+        elif saque > 0 and saque <= 500 and numero_saque <=3:
+            saldo -= saque
+            numero_saque += 1
+            print(f'Realizando saque de {saque:.2f}')
+            sleep(3)
+            print(f'''Saque de R$ {saque:.2f} realizado com sucesso!
+                  Saldo atual R$ {saldo:.2f}''')
+            chave = f'Saque: {numero_saque}'
+            valor = f"R$ {saque:.2f}"
+            extrato.update({chave:valor})
+            sleep(1)
+
+        else:
+            print('Verifique os valores digitados')
+        
+
+    
+    elif opcao.upper() == 'E':
+        print(f'\n\n{'-'*5}EXTRATO BANCÁRIO{'-'*5}')
+        print('IMPRIMINDO EXTRATO...')
+        sleep(3)
+        for key, value in extrato.items():
+            print(key, '→', value)
+        print(f'\n\nSaldo R${saldo:.2f}')
+
+    elif opcao.upper() == 'Q':
+        print('Encerrando sistema...')
+        break
+
+    else:
+>>>>>>> 19c69a3 (Adicionados alguns novos textos e import do sleep para deixar o funcionamento do programa mais parecido com uma transação real)
         print('Verifique a opção digitada e tente novamente.')
